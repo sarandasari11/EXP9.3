e05b5a38565f6dc5c05a7dd14baf50b05e08f557
Saran Dasari <dasarisaran2005@gmail.com>
2025-11-04 21:49:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("✅ Backend running successfully on AWS EC2!");
+});
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from Node.js Backend through Load Balancer!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
